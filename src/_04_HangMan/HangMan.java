910bc1236c58ef089b629fd4f363499ede7d8c64
League Student <league@tests-iMac.attlocal.net>
2019-06-21 18:24:29 -0700

    Hang Man (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,4 @@
 package _04_HangMan;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -9,11 +8,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
 public class HangMan implements KeyListener{
 	
-	//Lives start increasing if there are duplicate letters
-	//You can't win but you can lose haha :(( (need way to figure out that all letters were guessed)
+	//need to be able to start over!!!
+	//need to be able to separate multiple words in the wordLetters when you start over
+	//idea: make an int, first time you play that int is set to 0, as you play more that int++
+	//		every time you play again, the word at that int is pushed 
+	//		now how do you implement that :(
 	
 	Stack<String> words = new Stack<String>();
 	Stack<String> labelText = new Stack<String>();
@@ -24,6 +25,8 @@ public class HangMan implements KeyListener{
 	JPanel panel;
 	JLabel label;
 	String labeltext;
+	int letterCount = 0;
+	static boolean playAgain = true;
 	
 	public void hehe() {
 		String userInput = JOptionPane.showInputDialog("Enter a number between 1 and 266, inclusive.");
@@ -37,9 +40,7 @@ public class HangMan implements KeyListener{
 		for (int j = 0; j < randword.length(); j++) {
 			wordLetters.push(randword.charAt(j));
 		}
-		
 		System.out.println(randword);
-		
 		frame = new JFrame();
 		panel = new JPanel();
 		label = new JLabel();
@@ -53,7 +54,7 @@ public class HangMan implements KeyListener{
 		frame.addKeyListener(this);
 		frame.pack();
 		frame.setVisible(true);
-		JOptionPane.showMessageDialog(null, "Guess a letter!");
+		JOptionPane.showMessageDialog(null, "Guess a letter! You have " + lives + " chances to guess.");
 	}
 	
 	public void setLabelText() {
@@ -66,40 +67,46 @@ public class HangMan implements KeyListener{
 	
 	public static void main(String[] args) {
 		HangMan hehe = new HangMan();
-		hehe.hehe();
+		if (playAgain) {
+			hehe.hehe();
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		char letterGuess = e.getKeyChar();
+		boolean found = false;
 		for (int i = 0; i < wordLetters.size(); i++) {
 			if (wordLetters.get(i).equals(letterGuess) && labelText.get(i).contentEquals("_")) {
-				System.out.println("right");
 				labelText.remove(i);
-				String chara = Character.toString(wordLetters.get(i));
-				labelText.add((i), chara);
+				labelText.add((i), String.valueOf(letterGuess));
 				setLabelText();
-				lives++;
+				found = true;
 			}
 		}
-		if (lives > 0) {
+		if(!found) {
 			lives--;
-		} else {
-			JOptionPane.showMessageDialog(null, "Oh no! You lost.");
+			JOptionPane.showMessageDialog(null, "You lost a life! You have " + lives + " lives left.");
 		}
-		
-		System.out.println("Lives: " + lives);
+		if (labeltext.indexOf('_') < 0) {
+			int hmmQ = JOptionPane.showOptionDialog(null, "You won! Do you want to play again?", "YES OR NO!!!!!", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] {"Yes", "No"}, null);
+			if (hmmQ == 1) {
+				JOptionPane.showMessageDialog(null, "Oh, okay. Have a nice day!");
+				System.exit(0);
+			}
+		}
+		if (lives == 0) {
+			JOptionPane.showMessageDialog(null, "Oh no! You lost the game.");
+			JOptionPane.showMessageDialog(null, "The word was '" + randword + "'. Better luck next time!");
+			System.exit(0);
+		}
 	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 }
